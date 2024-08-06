2ff714cd8b62d30a91e40ce9d817e4e8c82b47d9
Jeff Patten <jeffspatten@gmail.com>
2024-08-06 14:08:17 -0700

    included winner calculator

--- App.js
+++ App.js
@@ -7,12 +7,36 @@ function App() {
   const [player, setPlayer] = useState(true);
 
   const handleClick = () => {
-    setSquares(['','','','','','','','','']);
+    setSquares(["", "", "", "", "", "", "", "", ""]);
     setPlayer(true);
-  }
+  };
+
+  const calculateWinner = (arr) => {
+    let lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} wins!`;
+      } else {
+        return `Who will win?`;
+      }
+    }
+  };
 
   return (
     <div className="App">
+      <span>{calculateWinner(squares)}</span>
       <div className="container">
         {squares.map((square, index) => {
           return (
